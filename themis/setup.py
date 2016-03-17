528246898dd52dae9000a92186d36e5e79c51760
W.P. McNeill <wmcneill@us.ibm.com>
2016-03-17 12:54:10 -0500

    Empty Themis project skeleton

---
+++ themis/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='themis',
+    version='1.0.0',
+    packages=['themis'],
+    url='https://github.ibm.com/WatsonTooling/data-science',
+    license='',
+    author='W.P. McNeill',
+    author_email='wmcneill@us.ibm.com',
+    description='Watson performance analysis toolkit'
+)
