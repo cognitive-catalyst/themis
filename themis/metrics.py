2cacb7a73cbffbe15eaebe09a5424ff2200999aa
stefan <stefan.vanderstockt@gmail.com>
2016-06-09 11:54:45 -0500

    Extracting questions_attempted to metrics.py similar to precision.  Avoids circular imports.

--- themis/metrics.py
+++ themis/metrics.py
@@ -10,3 +10,14 @@ def precision(judgments, t):
     except ZeroDivisionError:
         logger.warning("No in-purview questions at threshold level %0.3f" % t)
         return None
+
+
+def questions_attempted(judgments, t):
+    s = judgments[judgments[CONFIDENCE] >= t]
+    in_purview_attempted = sum(s[s[IN_PURVIEW]][FREQUENCY])
+    total_in_purview = sum(judgments[judgments[IN_PURVIEW]][FREQUENCY])
+    try:
+        return in_purview_attempted / float(total_in_purview)
+    except ZeroDivisionError:
+        logger.warning("No in-purview questions attempted at threshold level %0.3f" % t)
+        return None
